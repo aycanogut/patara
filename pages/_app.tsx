e518f7d281bd6975e0f98273a8001fd0777260ee
aycanogut <aycanonfire@gmail.com>
2025-05-10 13:52:07 +0300

    chore: :lipstick: add next fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import { Inter, Geist } from 'next/font/google';
+
+import '@/styles/globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['500', '600'],
+  variable: '--font-inter',
+});
+
+const geist = Geist({
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
+  variable: '--font-geist',
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${inter.className} ${geist.className}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <h1 className="font-inter text-4xl font-medium">Hello world</h1>
+      <h1 className="font-inter text-4xl font-semibold">Hello world</h1>
+      <h1 className="font-geist text-4xl font-normal">Hello world</h1>
+      <h1 className="font-geist text-4xl font-medium">Hello world</h1>
+      <h1 className="font-geist text-4xl font-semibold">Hello world</h1>
+    </div>
+  );
 }
