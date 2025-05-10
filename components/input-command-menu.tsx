e0fb8d6d685e14ae5c4062e2de0e7c2f3a695b09
aycanogut <aycanonfire@gmail.com>
2025-05-10 17:34:23 +0300

    feat: :sparkles: update input with command component

---
+++ components/input-command-menu.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+
+import { CommandDialog, CommandInput, CommandList, CommandEmpty, CommandGroup, CommandItem } from '@/components/ui/command';
+
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+
+function InputCommandMenu() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen(open => !open);
+      }
+    };
+    document.addEventListener('keydown', down);
+
+    return () => document.removeEventListener('keydown', down);
+  }, []);
+
+  return (
+    <div className="relative">
+      <Input
+        className="h-10 w-[29rem] rounded-xl border p-2 pr-24 pl-10"
+        placeholder="Enter Accounts, Platforms, NFTs, Token"
+        prependIconProps={{
+          name: 'search',
+          className: 'text-text-secondary',
+        }}
+      />
+
+      <Button
+        onClick={() => setOpen(true)}
+        className="bg-background-primary hover:bg-background-secondary hover:border-border-primary absolute top-1/2 right-2 h-6 w-8 -translate-y-1/2 rounded-lg border border-transparent px-1 py-2"
+      >
+        <kbd className="font-geist text-text-secondary text-xs font-medium">⌘K</kbd>
+      </Button>
+
+      <CommandDialog
+        open={open}
+        onOpenChange={setOpen}
+      >
+        <CommandInput placeholder="Bir komut arayın veya arama yapın..." />
+        <CommandList>
+          <CommandEmpty>Bir sonuç bulunamadı.</CommandEmpty>
+          <CommandGroup heading="Öneriler">
+            <CommandItem>Anasayfa</CommandItem>
+            <CommandItem>Hakkımızda</CommandItem>
+            <CommandItem>İletişim</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </div>
+  );
+}
+
+export default InputCommandMenu;
