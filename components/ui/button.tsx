8e48810289045a5c4c2eaff542fc6d55e5cfd4e4
aycanogut <aycanonfire@gmail.com>
2025-05-10 15:19:25 +0300

    refactor: :recycle: move button to components/ui folder

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,4 +1,9 @@
-import { cva } from 'class-variance-authority';
+import * as React from 'react';
+
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+import cn from '@/lib/utils/cn';
 
 const buttonVariants = cva(
   'inline-flex cursor-pointer items-center justify-center gap-2 text-sm whitespace-nowrap transition-colors focus-visible:ring-2 focus-visible:ring-text-primary  focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
@@ -24,4 +29,21 @@ const buttonVariants = cva(
   }
 );
 
-export default buttonVariants;
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, variant, size, emphasis, asChild = false, ...props }, ref) => {
+  const Comp = asChild ? Slot : 'button';
+  return (
+    <Comp
+      className={cn(buttonVariants({ variant, size, emphasis, className }))}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+
+Button.displayName = 'Button';
+
+export { Button, buttonVariants };
