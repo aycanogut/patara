cfb538dc79abe932d3974316c7042272fe9b1a5b
aycanogut <aycanonfire@gmail.com>
2025-05-10 18:54:53 +0300

    feat: :lipstick: update components responsive styles

--- components/input-command-menu.tsx
+++ components/input-command-menu.tsx
@@ -21,9 +21,9 @@ function InputCommandMenu() {
   }, []);
 
   return (
-    <div className="relative">
+    <div className="relative hidden md:block">
       <Input
-        className="h-10 w-[29rem] rounded-xl border p-2 pr-24 pl-10"
+        className="h-10 w-80 rounded-xl border p-2 pr-24 pl-10 lg:w-[29rem]"
         placeholder="Enter Accounts, Platforms, NFTs, Token"
         prependIconProps={{
           name: 'search',
@@ -33,6 +33,7 @@ function InputCommandMenu() {
 
       <Button
         onClick={() => setOpen(true)}
+        variant="ghost"
         className="bg-background-primary hover:bg-background-secondary hover:border-border-primary absolute top-1/2 right-2 h-6 w-8 -translate-y-1/2 rounded-lg border border-transparent px-1 py-2"
       >
         <kbd className="font-geist text-text-secondary text-xs font-medium">⌘K</kbd>
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -7,18 +7,20 @@ import cn from '@/lib/utils/cn';
 
 const buttonVariants = cva(
   cn(
-    'inline-flex cursor-pointer items-center justify-center gap-2 text-sm whitespace-nowrap transition-colors',
+    'inline-flex cursor-pointer items-center justify-center gap-2 text-xs md:text-sm whitespace-nowrap transition-colors',
     'focus-visible:ring-text-secondary focus-visible:ring-2 focus-visible:outline-none',
     'disabled:pointer-events-none disabled:opacity-50',
-    '[&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0'
+    '[&_svg]:pointer-events-none [&_svg]:size-6 [&_svg]:shrink-0'
   ),
   {
     variants: {
       variant: {
         default: 'bg-text-brand text-text-primary hover:bg-text-brand/80',
+        ghost: 'bg-transparent text-text-primary',
       },
       size: {
-        default: 'rounded-xl px-8 py-2.5',
+        default: 'rounded-xl p-2 lg:px-8 lg:py-2.5',
+        tight: 'p-0 md:p-0 lg:p-0',
         // icon: 'TODO'
       },
       emphasis: {
