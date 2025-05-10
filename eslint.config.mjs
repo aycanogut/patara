164b37ca1cf68d80a651eee2e35d5db196be06e3
aycanogut <aycanonfire@gmail.com>
2025-05-10 13:13:24 +0300

    build: :tada: init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,97 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const config = [
+  ...compat.extends('next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended', 'prettier'),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      prettier,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        JSX: 'readonly',
+        React: 'readonly',
+      },
+      parser: tsParser,
+    },
+    rules: {
+      'import/no-anonymous-default-export': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/display-name': 'off',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react/self-closing-comp': 'error',
+      'prettier/prettier': 'error',
+      'object-shorthand': 'error',
+      'no-restricted-imports': [
+        'warn',
+        {
+          paths: [
+            {
+              name: 'dayjs',
+              message: "Lütfen dayjs'i '@/utils/dayjs' üzerinden import edin.",
+            },
+          ],
+        },
+      ],
+      'react/jsx-curly-brace-presence': [
+        'error',
+        {
+          props: 'never',
+          children: 'never',
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'builtin',
+              position: 'after',
+            },
+            {
+              pattern: 'next/*',
+              group: 'external',
+              position: 'before',
+            },
+          ],
+          groups: ['builtin', 'external', 'type', 'object', 'internal', 'parent', 'sibling', 'index'],
+          pathGroupsExcludedImportTypes: ['react', 'next'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+          },
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      '@next/next/no-img-element': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+    },
+  },
 ];
 
-export default eslintConfig;
+export default config;
