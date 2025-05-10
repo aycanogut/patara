3e2225c16dc59cd6b5c9040e43f1dc06ab2d57e1
aycanogut <aycanonfire@gmail.com>
2025-05-10 17:34:33 +0300

    feat: :sparkles: add demo component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,12 @@
+import React from 'react';
+
+import InputCommandMenu from '@/components/input-command-menu';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center gap-10 bg-black p-20">
-      <Input
-        className="h-10 w-[29rem] rounded-xl border p-2 pl-10"
-        placeholder="Enter Accounts, Platforms, NFTs, Token"
-        prependIconProps={{
-          name: 'search',
-          className: 'text-text-secondary',
-        }}
-      />
-
+      <InputCommandMenu />
       <Button emphasis="strong">Connect/Sign in</Button>
     </div>
   );
