5cf24d0082485684c115bbf7d0ce50ce7868db4d
aycanogut <aycanonfire@gmail.com>
2025-05-11 23:53:31 +0300

    refactor: :fire: remove unnecessary class

--- pages/referral-connect/index.tsx
+++ pages/referral-connect/index.tsx
@@ -6,7 +6,7 @@ import ReferralCards from './ReferralCards';
 function ReferralConnect() {
   return (
     <Layout meta={{ title: 'Referral Connect' }}>
-      <section className="bg-background-default container mx-auto flex h-auto max-w-[87.5rem] flex-col items-center gap-10 overflow-x-hidden px-4 pt-5 pb-28 md:px-4 xl:flex-row xl:justify-between xl:gap-4 2xl:px-0">
+      <section className="bg-background-default container mx-auto flex max-w-[87.5rem] flex-col items-center gap-10 overflow-x-hidden px-4 pt-5 pb-28 md:px-4 xl:flex-row xl:justify-between xl:gap-4 2xl:px-0">
         <ReferralHero />
         <ReferralCards />
       </section>
