4aee0620963ddebb3e6b251ad3b0c4d1916d9c0a
aycanogut <aycanonfire@gmail.com>
2025-05-12 00:00:58 +0300

    fix: :green_heart: fix build error

--- pages/referral-connect/ReferralCards/index.tsx
+++ pages/referral-connect/ReferralCards/index.tsx
@@ -1,5 +1,34 @@
-import InfoCard from './info-card';
-import mockData from './mockData';
+import InfoCard, { InfoCardProps } from './info-card';
+
+export const mockData: InfoCardProps[] = [
+  {
+    id: 1,
+    label: 'Total Earned Fee',
+    value: '$1,000.00',
+    icon: 'hand-coins',
+  },
+  {
+    id: 2,
+    label: 'Unclaimed Fee',
+    value: '$500.00',
+    icon: 'asterisk',
+    buttonProps: {
+      children: 'Claim',
+    },
+  },
+  {
+    id: 3,
+    label: 'Total Referral Points',
+    value: '1289',
+    icon: 'gift',
+  },
+  {
+    id: 4,
+    label: 'Referrals',
+    value: '34',
+    icon: 'user-circle',
+  },
+];
 
 function ReferralCards() {
   return (
