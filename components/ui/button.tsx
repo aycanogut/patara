5969da8d45c10cb274c10f4841f51632148a934b
aycanogut <aycanonfire@gmail.com>
2025-05-11 14:45:21 +0300

    fix: :lipstick: update responsive styles

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -7,7 +7,7 @@ import cn from '@/lib/utils/cn';
 
 const buttonVariants = cva(
   cn(
-    'inline-flex max-w-min cursor-pointer items-center justify-center gap-2 text-xs whitespace-nowrap transition-colors md:text-sm',
+    'inline-flex max-w-min cursor-pointer items-center justify-center gap-2 text-xs whitespace-nowrap transition-colors sm:text-sm',
     'focus-visible:ring-text-secondary focus-visible:ring-2 focus-visible:outline-none',
     'disabled:pointer-events-none disabled:opacity-50',
     '[&_svg]:pointer-events-none [&_svg]:size-6 [&_svg]:shrink-0'
@@ -19,8 +19,8 @@ const buttonVariants = cva(
         ghost: 'bg-transparent text-text-primary',
       },
       size: {
-        default: 'rounded-xl p-2 lg:px-8 lg:py-2.5',
-        md: 'rounded-2xl p-2 lg:px-4 lg:py-3.5',
+        default: 'rounded-lg p-2 lg:rounded-xl lg:px-8 lg:py-2.5',
+        md: 'rounded-xl p-2 lg:rounded-2xl lg:px-4 lg:py-3.5',
         tight: 'p-0 md:p-0 lg:p-0',
         // icon: 'TODO'
       },
