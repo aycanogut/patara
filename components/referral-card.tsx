c6f5a55769ec34ab5321517629ad64f4909865d2
aycanogut <aycanonfire@gmail.com>
2025-05-11 14:20:16 +0300

    feat: :sparkles: update referral card with orbit elements

--- components/referral-card.tsx
+++ components/referral-card.tsx
@@ -1,11 +1,14 @@
+import OrbitAvatars from '@/components/orbit-avatars/orbit-avatars';
 import { Button } from '@/components/ui/button';
 
 function ReferralCard() {
   return (
-    <article className="bg-background-primary border-border-primary flex max-h-[40.1875rem] max-w-[29.125rem] flex-col rounded-3xl border px-5 py-10">
-      <div className="flex flex-col items-center gap-9">
-        <header className="flex flex-col gap-5">
-          <h1 className="text-text-primary text-center text-2xl leading-4 font-medium">Refer friends and earn with Patara!</h1>
+    <article className="bg-background-primary border-border-primary group mx-3 flex max-w-[29.125rem] flex-col items-center gap-10 overflow-hidden rounded-3xl border px-4 py-10 sm:mx-0 md:gap-20">
+      <OrbitAvatars />
+
+      <div className="flex flex-col items-center gap-10">
+        <header className="mt-2 flex flex-col gap-3 md:gap-5">
+          <h1 className="text-text-primary text-center text-2xl font-medium md:leading-4">Refer friends and earn with Patara!</h1>
           <p className="text-text-secondary px-2 text-center text-base leading-6">
             Invite your friends to Patara and earn a share of their on-chain rewards forever!
           </p>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,9 @@ import Layout from '@/layout';
 export default function Home() {
   return (
     <Layout meta={{ title: 'Anasayfa' }}>
-      <div className="flex h-full items-center justify-center">
+      <section className="flex h-full items-center justify-center">
         <ReferralCard />
-      </div>
+      </section>
     </Layout>
   );
 }
