caf1939e7dcc0501f8b7c49993fc5cb598c92a32
aycanogut <aycanonfire@gmail.com>
2025-05-12 21:17:10 +0300

    fix: :bug: responsive styles

--- pages/(home)/referral-card.tsx
+++ pages/(home)/referral-card.tsx
@@ -5,11 +5,11 @@ import { Badge } from '@/components/ui/badge';
 
 function ReferralCard() {
   return (
-    <article className="md:bg-background-primary md:border-border-default group z-10 mx-3 flex max-w-[29.125rem] flex-col items-center gap-5 overflow-hidden rounded-3xl border border-transparent px-2 py-10 sm:mx-0 md:gap-20 md:px-4">
+    <article className="md:bg-background-primary md:border-border-default group z-10 mx-3 flex max-w-[29.125rem] flex-col items-center gap-5 overflow-hidden rounded-3xl border border-transparent px-2 py-10 sm:mx-0 md:mt-16 md:gap-20 md:px-4 lg:mt-18">
       <OrbitAvatars />
 
       <div className="flex flex-col items-center gap-5 md:gap-10">
-        <header className="mt-2 flex flex-col gap-3 md:gap-5">
+        <header className="mt-10 flex flex-col gap-3 md:mt-2 md:gap-5">
           <h1 className="text-text-primary text-center text-2xl font-medium md:leading-4">Refer friends and earn with Patara!</h1>
           <p className="text-text-secondary px-2 text-center text-base leading-6">
             Invite your friends to Patara and earn a share of their on-chain rewards forever!
